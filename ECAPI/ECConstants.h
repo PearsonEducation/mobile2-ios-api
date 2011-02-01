8cf85748f1bb7719ed7606e14387dece591f824f
Tony Hillerson <tony.hillerson@effectiveui.com>
2011-02-01 16:23:00 -0700

    Adding a constants file and importing in precompiled header

---
+++ ECAPI/ECConstants.h
@@ -0,0 +1,9 @@
+//
+//  ECConstants.h
+//  ECAPI
+//
+//  Created by Tony Hillerson on 2/1/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#define M_API_URL @"https://m-api.ecollege.com"
