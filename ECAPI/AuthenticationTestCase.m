34ed6491679d546474f819afb4f24f632511a6e0
Tony Hillerson <tony.hillerson@effectiveui.com>
2011-02-01 21:08:12 -0700

    Changing to running the authentication test the GHUnit Async way

--- ECAPI/AuthenticationTestCase.m
+++ ECAPI/AuthenticationTestCase.m
@@ -6,34 +6,32 @@
 //  Copyright 2011 EffectiveUI. All rights reserved.
 //
 
-#import "AuthenticationTestCase.h"
 #import "ECSession.h"
+
+@interface AuthenticationTestCase : GHAsyncTestCase<ECSessionAuthenticationDelegate> {
+	ECSession *session;
+}
+@end
 
 @implementation AuthenticationTestCase
 
 - (void) dealloc {
-	[session release]; session = nil;
+	[session release];
 }
 
 - (void) testAuthentication {
-	// we need to run this test on the main thread:
-	[self performSelectorOnMainThread:@selector(runAuthenticationTest) withObject:nil waitUntilDone:YES];
-}
-
-- (void) runAuthenticationTest {
-	authenticationTestCompleted = NO;
+	[self prepare];
+	
 	session = [[ECSession alloc] initWithClientString:@"sandbox" username:@"manderson" password:@"manderson"];
 	session.authenticationDelegate = self;
 	[session authenticate];
-	// Wait a few seconds
-	[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2.0]];
-	GHAssertTrue(authenticationTestCompleted, @"Authentication test did not complete");
+
+	[self waitForStatus:kGHUnitWaitStatusSuccess timeout:10.0];
 }
 
 - (void) sessionDidAuthenticate:(ECSession *)aSession {
-	authenticationTestCompleted = YES;
-	GHAssertEqualObjects(session, aSession, @"Session Test Failure: unexpected session returned to delegate");
-	GHAssertNotNil(session.accessToken, @"Expected accessToken to be set after authentication");
+	GHAssertNotNil(aSession.accessToken, @"Expected accessToken to be set after authentication");
+	[self notify:kGHUnitWaitStatusSuccess forSelector:@selector(testAuthentication)];
 }
 
 @end
