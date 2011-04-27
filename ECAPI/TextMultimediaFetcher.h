5787761677b3e4dfa4ccccc1ae8873fd48fed78e
Tony Hillerson <tony.hillerson@gmail.com>
2011-04-27 16:32:30 -0600

    Adding an HTML content fetcher.

---
+++ ECAPI/TextMultimediaFetcher.h
@@ -0,0 +1,18 @@
+//
+//  TextMultimediaFetcher.h
+//  ECAPI
+//
+//  Created by Tony Hillerson on 4/27/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ECAuthenticatedFetcher.h"
+
+@interface TextMultimediaFetcher : ECAuthenticatedFetcher {
+    
+}
+
+- (void) fetchHTMLContentForCourseId:(NSInteger)courseId contentId:(NSNumber *)contentId;
+
+@end
---
+++ ECAPI/TextMultimediaFetcher.m
@@ -0,0 +1,22 @@
+//
+//  TextMultimediaFetcher.m
+//  ECAPI
+//
+//  Created by Tony Hillerson on 4/27/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#import "TextMultimediaFetcher.h"
+
+@implementation TextMultimediaFetcher
+
+- (void) fetchHTMLContentForCourseId:(NSInteger)courseId contentId:(NSNumber *)contentId {
+	NSString *url = [NSString stringWithFormat:@"%@/courses/%d/textMultimedias/%qu/content.html", M_API_URL, courseId, [contentId longLongValue]];
+	[self loadDataFromURLString:url withDeserializationSelector:@selector(deserializeHTML:)];
+}
+	
+- (id) deserializeHTML:(id)parsedData {
+	return parsedData;
+}
+
+@end
